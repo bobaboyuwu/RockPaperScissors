cb5eb1d463e4c8a812d6210bcf25fd5f9bbea0fd
Jordan <jordanlaxa22@gmail.com>
2022-12-07 00:32:18 -0800

    Add first edits to script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello Everyone Again Part 2!");
+var word = ['Rock', 'Paper', 'Scissors'];
+var getComputerChoice = word[Math.floor(Math.random()*word.length)];
+let playerSelection = prompt("What do you choose?", "Enter Rock, Paper, or Scissors" )
+console.log(getComputerChoice);
+let text;
+switch(playerSelection) {
+    case "Scissors"
+    if (getComputerChoice = "Scissors") {
+    alert ("You Win! Rock Beats Scissor!");
+    } else if (getComputerChoice = "Paper") {
+        alert ("You Lose! Paper Beats Rock!");
+    } else {
+        alert ("Draw! Rock Doesn't Beat Rock!");
+    }
+    }
+}
+console.log (playRound);
