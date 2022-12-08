2b889445204b62bfd16db4f437fd66adb508a99c
Jordan <jordanlaxa22@gmail.com>
2022-12-08 00:21:31 -0800

    Refine the getComputerChoice function and add outcomes

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 console.log("Hello Everyone Again Part 2!");
-var word = ['Rock', 'Paper', 'Scissors'];
-var getComputerChoice = word[Math.floor(Math.random()*word.length)];
-let playerSelection = prompt("What do you choose?", "Enter Rock, Paper, or Scissors" )
-console.log(getComputerChoice);
-let text;
-switch(playerSelection) {
-    case "Scissors"
-    if (getComputerChoice = "Scissors") {
-    alert ("You Win! Rock Beats Scissor!");
-    } else if (getComputerChoice = "Paper") {
-        alert ("You Lose! Paper Beats Rock!");
-    } else {
-        alert ("Draw! Rock Doesn't Beat Rock!");
-    }
-    }
+function getComputerChoice ()  {
+    var words = ['Rock', 'Paper', 'Scissors'];
+    var computerSelection = words[Math.floor(Math.random()*words.length)];
+    return computerSelection;
 }
-console.log (playRound);
+
+console.log(getComputerChoice());
+let playerSelection = prompt("What do you choose?", "Enter Rock, Paper, or Scissors" )
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowercase();
+    if (computerSelection == playerSelection) {
+        alert("It's a tie!");
+    } else if (
+        (computerSelection == "rock" && playerSelection =="scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")) {
+        alert("You lose! Try again!");
+    } else if (
+        (computerSelection == "scissors" && playerSelection =="rock") ||
+        (computerSelection == "paper" && playerSelection == "scissors") ||
+        (computerSelection == "rock" && playerSelection == "paper")) {
+        alert("You win! Good job!");
+        }
+    
+}
+console.log(playRound());
+// 1.  have computer randomly select rock, paper, or scissors
+// 2. allow player to choose rock, paper, or scissors
+// 3. compare choices
+// 4. declare who wins
