b3b98e7fe0070de44d654b59db8d8e1c84b571eb
Jordan <jordanlaxa22@gmail.com>
2023-01-26 21:34:58 -0800

    Add the complete UI layout

--- script.js
+++ script.js
@@ -1,58 +1,84 @@
-// console.log("Hello Everyone Again Part 2!");
-var computerSelection;
-var words;
-let computerScore = 0;
-let playerScore = 0;
 function getComputerChoice ()  {
     words = ['rock', 'paper', 'scissors'];
     computerSelection = words[Math.floor(Math.random()*words.length)];
     return computerSelection;
 }
+let playerScore = 0;
+let computerScore = 0;
+const container = document.querySelector('#container');
+const plaScore = document.querySelector('#plaScore');
+const comScore = document.querySelector('#comScore');
+const res = document.querySelector('#res');
+const winnerDec = document.querySelector('#winnerDec');
+const winner = document.createElement('div');
+const results = document.createElement('div');
+const playScore = document.createElement('div');
+const compScore = document.createElement('div');
+const toFive = document.createElement('div');
+
+var paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+playRound(playerSelection="paper", computerSelection = getComputerChoice);
+});
+var rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+playRound(playerSelection="rock", computerSelection = getComputerChoice);
+ });
+var scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+ playRound(playerSelection="scissors", computerSelection = getComputerChoice);
+  });
+
 
 function playRound(playerSelection, computerSelection) {
     //console.log(getComputerChoice());
     //computerSelection = getComputerChoice().toLowerCase();
     computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    playerSelection = prompt("What do you choose?", "Enter Rock, Paper, or Scissors" );
-    console.log(playerSelection);
     if (computerSelection == playerSelection) {
-        alert("It's a tie!");
+            results.textContent = "It's a tie!";
+            res.appendChild(results);
+            playScore.textContent = (playerScore);
+            compScore.textContent = (computerScore);
+        plaScore.appendChild(playScore);
+        comScore.appendChild(compScore);
     } else if (
         (computerSelection == "rock") && (playerSelection =="scissors") ||
         (computerSelection == "scissors") && (playerSelection == "paper")||
         (computerSelection == "paper") && (playerSelection == "rock")) {
-        alert("You lose! Try again!");
-        computerScore = ++computerScore;
-        console.log(computerScore);
+            
+            results.textContent = 'You lose, try again!';
+            res.appendChild(results);
+        ++computerScore;
+        playScore.textContent = (playerScore);
+        compScore.textContent = (computerScore);
+        plaScore.appendChild(playScore);
+        comScore.appendChild(compScore);
 
-        ;
     } else if (
         (computerSelection == "scissors" && playerSelection =="rock") ||
         (computerSelection == "paper" && playerSelection == "scissors") ||
         (computerSelection == "rock" && playerSelection == "paper")) {
-        alert("You win! Good job!");
-        playerScore = ++playerScore;
-        console.log(playerScore);
-        } else 
-        alert("Not able to process choice");
-        if (playerScore === 5 || computerScore === 5) {
-            declareWinner();
+            
+            results.textContent = 'You win! Congrats!';
+            res.appendChild(results);
+        ++playerScore;
+        playScore.textContent = (playerScore);
+        compScore.textContent = (computerScore);
+        plaScore.appendChild(playScore);
+        comScore.appendChild(compScore);
+        } else {
+            console.log(playerScore);
+        };
+
+        if (playerScore == 5) {
+            winner.textContent = 'Congrats, you won 5 rounds!';
+            winnerDec.appendChild(winner);
         }
-        function declareWinner() {
-            if (playerScore > computerScore) {
-                console.log("You beat the computer in 5 rounds!");
-            }
-                else {
-                    console.log("You lost! The computer has won 5 rounds!")
-                }
-            }
-         }
-
-
-// 1.  have computer randomly select rock, paper, or scissors
-// 2. allow player to choose rock, paper, or scissors
-// 3. compare choices
-// 3a. log who wins
-// 3b. when 5 wins happens, display a message
-// 4. declare who wins
+        else if (computerScore == 5) {
+            winner.textContent = 'The computer has won 5 rounds!';
+            winnerDec.appendChild(winner);
+        }
+        else {
+            console.log("hi");
+        }
+    }
